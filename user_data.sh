74eefad7fdea81ae88c683b2845f98650be48dd4
Pedro Gabriel Modesto Caddah <122160819+JKLModesto@users.noreply.github.com>
2024-12-24 02:13:09 -0300

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -4,6 +4,15 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install -y docker.io
+sudo apt-get install -y mysql-client
+sudo apt install -y amazon-efs-utils
+sudo apt install -y nfs-common
+
+# Cria o diretório efs 
+sudo mkdir -p /mnt/efs
+
+#  Monta um sistema de arquivos da Amazon Elastic File System (EFS) no Linux
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport <id fornecido pelo EFS>:/ /mnt/efs
 
 # Instalar docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -30,17 +39,42 @@ services:
     ports:
       - "80:80"
     environment:
-      WORDPRESS_DB_HOST: <host>:3306
+      WORDPRESS_DB_HOST: <id fornecido pelo RDS>:3306
       WORDPRESS_DB_USER: admin
-      WORDPRESS_DB_PASSWORD: password
+      WORDPRESS_DB_PASSWORD: <senha do banco de dados>
       WORDPRESS_DB_NAME: wordpress
     volumes:
-      - wordpress_data:/var/www/html
+      - /mnt/efs:/var/www/html
 
-volumes:
-  wordpress_data:
 EOL
 
 # Inicia o Docker Compose
 docker-compose up -d
 
+# Aguarda o container WordPress estar ativo
+echo "Aguardando o container WordPress iniciar..."
+until sudo docker ps | grep -q "Up.*wordpress"; do
+  echo "Verificando containers em execução..."
+  sudo docker ps
+  sleep 5
+done
+echo "Container WordPress iniciado!"
+
+
+# Adiciona o arquivo healthcheck.php no container WordPress
+echo "Criando o arquivo healthcheck.php no container WordPress..."
+sudo docker exec -i wordpress bash -c "cat <<EOF > /var/www/html/healthcheck.php
+<?php
+http_response_code(200);
+header('Content-Type: application/json');
+echo json_encode([\"status\" => \"OK\", \"message\" => \"Health check passed\"]);
+exit;
+?>
+EOF"
+
+# Confirma a criação do arquivo
+if docker exec -i wordpress ls /var/www/html/healthcheck.php > /dev/null 2>&1; then
+  echo "Arquivo healthcheck.php criado com sucesso!"
+else
+  echo "Falha ao criar o arquivo healthcheck.php."
+fi
